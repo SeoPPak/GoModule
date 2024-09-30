bd36eff3dcfe55485c4d30234eaea06a12309ebb
SeoPPak <andrew201239@gmail.com>
2024-09-30 18:35:00 +0900

    initialize

---
+++ tree.go
@@ -0,0 +1,42 @@
+package tree
+
+import (
+	"fmt"
+)
+
+// Node 구조체는 이진 탐색 트리의 노드를 정의합니다.
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+func InsertNode(num int) *Node {
+	return &Node{Value: num}
+}
+
+// Insert 함수는 이진 탐색 트리에 새로운 값을 삽입하는 함수입니다.
+func (tree *Node) Insert(num int) {
+	if num < tree.Value {
+		if tree.Left == nil {
+			tree.Left = InsertNode(num)
+		} else {
+			tree.Left.Insert(num)
+		}
+	} else {
+		if tree.Right == nil {
+			tree.Right = InsertNode(num)
+		} else {
+			tree.Right.Insert(num)
+		}
+	}
+}
+
+func InOrder(n *Node) {
+	if n == nil {
+		return
+	}
+	InOrder(n.Left)
+	fmt.Print(n.Value, " ")
+	InOrder(n.Right)
+}
